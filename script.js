bcc83492f98f8445b7466ee1637d2c53814c7a1f
SerhiiYu <sergeyuhimenko@gmail.com>
2021-05-26 18:33:15 +0300

    new data

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 'use strict';
 
 // Data
+
 const account1 = {
-  owner: 'Jonas Schmedtmann', //"js" username
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -13,23 +27,23 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+const accounts = [account1, account2];
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -68,7 +82,7 @@ const displayMovements = function (acc, sort = false) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov} €</div>
+      <div class="movements__value">${mov.toFixed(2)} €</div>
     </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
@@ -77,7 +91,7 @@ const displayMovements = function (acc, sort = false) {
 //Balance
 const calcDisplayBalance = acc => {
   acc.balance = acc.movements.reduce((acc, cur) => acc + cur);
-  labelBalance.textContent = `${acc.balance} €`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} €`;
 };
 
 //Summary
@@ -85,7 +99,7 @@ const calcDisplaySummary = acc => {
   const income = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, cur) => acc + cur);
-  labelSumIn.textContent = income + ' €';
+  labelSumIn.textContent = income.toFixed(2) + ' €';
   let withdrawal;
   if (acc.movements.some(mov => mov < 0)) {
     withdrawal = acc.movements
@@ -94,13 +108,13 @@ const calcDisplaySummary = acc => {
   } else {
     withdrawal = 0;
   }
-  labelSumOut.textContent = -withdrawal + ' €';
+  labelSumOut.textContent = -withdrawal.toFixed(2) + ' €';
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(mov => (mov * acc.interestRate) / 100)
     .filter(mov => mov >= 1)
     .reduce((acc, cur) => acc + cur);
-  labelSumInterest.textContent = interest + ' €';
+  labelSumInterest.textContent = interest.toFixed(2) + ' €';
 };
 
 const display = acc => {
@@ -202,43 +216,43 @@ btnSort.addEventListener('click', e => {
 
 // LECTURES
 // ex 1
-const sum = accounts
-  .flatMap(acc => acc.movements)
-  .filter(mov => mov > 0)
-  .reduce((a, b) => a + b);
-console.log(sum);
-//ex 2
-const oneThousand = accounts
-  .flatMap(acc => acc.movements)
-  .reduce((a, b) => (b >= 1000 ? ++a : a), 0);
-console.log(oneThousand);
-//ex 3
-const { deposit, withdrawal } = accounts
-  .flatMap(acc => acc.movements)
-  .reduce(
-    (acc, cur) => {
-      acc[cur > 0 ? 'deposit' : 'withdrawal'] += cur;
-      return acc;
-    },
-    {
-      deposit: 0,
-      withdrawal: 0,
-    }
-  );
-console.log(deposit, withdrawal);
-// ex 4
-const convertTitle = title => {
-  const capitalize = str => str[0].toUpperCase()+str.slice(1);
-  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
-  return capitalize(
-    title
-      .toLowerCase()
-      .split(' ')
-      .map(str => (exceptions.includes(str) ? str : capitalize(str)))
-      .join(' ')
-  );
-};
-console.log(convertTitle('aNd this iS A TiTlE'));
+// const sum = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov > 0)
+//   .reduce((a, b) => a + b);
+// console.log(sum);
+// //ex 2
+// const oneThousand = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((a, b) => (b >= 1000 ? ++a : a), 0);
+// console.log(oneThousand);
+// //ex 3
+// const { deposit, withdrawal } = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce(
+//     (acc, cur) => {
+//       acc[cur > 0 ? 'deposit' : 'withdrawal'] += cur;
+//       return acc;
+//     },
+//     {
+//       deposit: 0,
+//       withdrawal: 0,
+//     }
+//   );
+// console.log(deposit, withdrawal);
+// // ex 4
+// const convertTitle = title => {
+//   const capitalize = str => str[0].toUpperCase()+str.slice(1);
+//   const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+//   return capitalize(
+//     title
+//       .toLowerCase()
+//       .split(' ')
+//       .map(str => (exceptions.includes(str) ? str : capitalize(str)))
+//       .join(' ')
+//   );
+// };
+// console.log(convertTitle('aNd this iS A TiTlE'));
 
 
 
